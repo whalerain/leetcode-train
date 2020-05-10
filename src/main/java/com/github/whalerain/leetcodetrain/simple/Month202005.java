5b0f4e8a64751805af8820284332d5fb38d67a3c
Jason Zhang <zhangxi.work@foxmail.com>
2020-05-11 07:47:56 +0800

    temp: 20200511

---
+++ src/main/java/com/github/whalerain/leetcodetrain/simple/Month202005.java
@@ -0,0 +1,34 @@
+package com.github.whalerain.leetcodetrain.simple;
+
+/**
+ * @author ZhangXi
+ */
+public class Month202005 {
+
+    /**
+     * 1013. 将数组分成和相等的三个部分
+     * https://leetcode-cn.com/problems/partition-array-into-three-parts-with-equal-sum/
+     */
+    public static boolean canThreePartsEqualSum(int[] array) {
+        //todo 依次相加对比
+        if (null == array || array.length < 3) {
+            return false;
+        }
+
+        // 首位相加
+        for (int i=0; i<array.length; i++) {
+
+
+
+        }
+
+
+
+        return false;
+    }
+
+
+
+
+
+}
---
+++ src/test/java/com/github/whalerain/leetcodetrain/simple/Month202005Test.java
@@ -0,0 +1,20 @@
+package com.github.whalerain.leetcodetrain.simple;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author ZhangXi
+ */
+public class Month202005Test {
+
+    @Test
+    public void testCanThreePartsEqualSum() {
+
+    }
+
+
+
+
+
+}
