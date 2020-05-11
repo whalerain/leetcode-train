59a19565fd4b04325646344dd96f7628cde025c4
Jason Zhang <zhangxi.work@foxmail.com>
2020-05-12 07:47:37 +0800

    add: 完成 - 1013. 将数组分成和相等的三个部分

--- src/main/java/com/github/whalerain/leetcodetrain/simple/Month202005.java
+++ src/main/java/com/github/whalerain/leetcodetrain/simple/Month202005.java
@@ -10,19 +10,33 @@ public class Month202005 {
      * https://leetcode-cn.com/problems/partition-array-into-three-parts-with-equal-sum/
      */
     public static boolean canThreePartsEqualSum(int[] array) {
-        //todo 依次相加对比
         if (null == array || array.length < 3) {
             return false;
         }
 
-        // 首位相加
+        // 计算总和
+        int total = 0;
         for (int i=0; i<array.length; i++) {
-
-
-
+            total += array[i];
+        }
+        if (total % 3 != 0) {
+            return false;
         }
 
-
+        // 依次计算前两部分组合值
+        int sum = total / 3, top = 0;
+        for (int i=0; i<array.length-2; i++) {
+            top += array[i];
+            if (top == sum) {
+                int mid = 0;
+                for (int j=i+1; j<array.length-1; j++) {
+                    mid += array[j];
+                    if (mid == sum) {
+                        return true;
+                    }
+                }
+            }
+        }
 
         return false;
     }
--- src/test/java/com/github/whalerain/leetcodetrain/simple/Month202005Test.java
+++ src/test/java/com/github/whalerain/leetcodetrain/simple/Month202005Test.java
@@ -1,16 +1,19 @@
 package com.github.whalerain.leetcodetrain.simple;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
- *
+ * 2020年5月测试
  * @author ZhangXi
  */
 public class Month202005Test {
 
     @Test
     public void testCanThreePartsEqualSum() {
-
+        int[] array = {0,2,1,-6,6,7,9,-1,2,0,1};
+        boolean result = Month202005.canThreePartsEqualSum(array);
+        Assert.assertFalse("预期=false", result);
     }
 
 
